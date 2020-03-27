c1d2965ff5e20edbe87ecc0f5833f68db4f56c65
Arjun <arjunkushwah@hotmail.com>
2020-03-27 06:00:33 +0530

    modif

--- src/main/java/problems/NumOfIslands.java
+++ src/main/java/problems/NumOfIslands.java
@@ -1,7 +1,17 @@
 package problems;
 
 class NumOfIslands {
-	public int numIslands(char[][] grid) {
+	public static void main(String args[]) {
+		char arr[][]={
+				   {'1','1','0','0','0'},
+	               {'0','1','1','0','0'},
+	               {'0','0','1','0','1'},
+	               {'1','0','0','0','1'},
+	               {'0','1','0','1','1'}
+	               };
+		System.out.println(numIslands(arr));
+	}
+	public static int numIslands(char[][] grid) {
 		if (grid == null || grid.length == 0) {
 			return 0;
 		}
@@ -16,15 +26,18 @@ class NumOfIslands {
 		return numIslands;
 	}
 
-	public int dfs(char[][] grid, int i, int j) {
-		if (i < 0 || j < 0 || grid[i][j] == '0' || i <= grid.length || j<grid[i].length) {
+	public static int dfs(char[][] grid, int i, int j) {
+		if (i < 0 || j < 0  || i >= grid.length || j>=grid[i].length || grid[i][j] == '0') {
 			return 0;
-		}
+		}else {
 		grid[i][j] = '0';
-		dfs(grid, i, j + 1);
 		dfs(grid, i + 1, j);
-		dfs(grid, i, j - 1);
 		dfs(grid, i - 1, j);
+		dfs(grid, i, j + 1);
+		
+		dfs(grid, i, j - 1);
+		
 		return 1;
+		}
 	}
 }
