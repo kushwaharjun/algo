8739e98eff199dfb3dba11e038f2b7ae0bad9366
Arjun kushwah <53173361+kushwaharjun@users.noreply.github.com>
2020-04-03 09:50:19 +0530

    Update PrintSpiralMatrix.java

--- src/main/java/problems/PrintSpiralMatrix.java
+++ src/main/java/problems/PrintSpiralMatrix.java
@@ -1,13 +1,46 @@
 package problems;
 
 public class PrintSpiralMatrix {
-	public void print(int a[][]) {
-		
-	}
-
-	public static void main(String[] args) {
-		int a[][] = { { 1, 2, 3, 4, 5, 6 }, { 7, 8, 9, 10, 11, 12 }, { 13, 14, 15, 16, 17, 18 } };
-
-	}
+	vector<int> Solution::spiralOrder(const vector<vector<int> > &A) {
+    
+    int T,B,L,R,dir;
+    T=0;
+    B=A.size()-1;
+    L=0;
+    R=A[0].size()-1;
+    dir=0;
+    int i;
+    vector<int> ans;
+    
+    while(T<=B && L<=R)
+    {
+        if(dir==0)
+        {
+            for(i=L;i<=R;i++)
+                ans.push_back(A[T][i]);
+            T++;
+        }
+        else if(dir==1)
+        {
+            for(i=T;i<=B;i++)
+                ans.push_back(A[i][R]);
+            R--;
+        }
+        else if(dir==2)
+        {
+            for(i=R;i>=L;i--)
+                ans.push_back(A[B][i]);
+            B--;
+        }
+        else if(dir==3)
+        {
+            for(i=B;i>=T;i--)
+                ans.push_back(A[i][L]);
+            L++;
+        }
+        dir=(dir+1)%4;
+    }
+    return ans;
+}
 
 }
