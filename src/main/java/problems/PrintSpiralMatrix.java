087d807eba980419692b80e2218bf1f7967d4c18
Arjun kushwah <53173361+kushwaharjun@users.noreply.github.com>
2020-04-03 09:56:27 +0530

    Update PrintSpiralMatrix.java

--- src/main/java/problems/PrintSpiralMatrix.java
+++ src/main/java/problems/PrintSpiralMatrix.java
@@ -44,3 +44,12 @@ public class PrintSpiralMatrix {
 }
 
 }
+/**
+Top(T) -> |1 2 3 4 5|
+          |6 7 8 9 7|
+          |4 7 1 8 0|
+bot(B) -> |9 2 6 2 4|
+           ^       ^
+           |       |
+
+      Left(L)  right(R)
