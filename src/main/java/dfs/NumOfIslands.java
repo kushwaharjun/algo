08bdb491ed65be43f2d35cb242b7840c9fa2f216
Arjun kushwah <53173361+kushwaharjun@users.noreply.github.com>
2022-02-18 09:10:15 +0530

    Update NumOfIslands.java

--- src/main/java/dfs/NumOfIslands.java
+++ src/main/java/dfs/NumOfIslands.java
@@ -1,16 +1,6 @@
 package problems;
 
 class NumOfIslands {
-	public static void main(String args[]) {
-		char arr[][]={
-	               {'1','1','0','0','0'},
-	               {'0','1','1','0','0'},
-	               {'0','0','1','0','1'},
-	               {'1','0','0','0','1'},
-	               {'0','1','0','1','1'}
-	               };
-		System.out.println(numIslands(arr));
-	}
 	public static int numIslands(char[][] grid) {
 		if (grid == null || grid.length == 0) {
 			return 0;
@@ -33,11 +23,19 @@ class NumOfIslands {
 		grid[i][j] = '0';
 		dfs(grid, i + 1, j);
 		dfs(grid, i - 1, j);
-		dfs(grid, i, j + 1);
-		
-		dfs(grid, i, j - 1);
-		
+		dfs(grid, i, j + 1);		
+		dfs(grid, i, j - 1);		
 		return 1;
 		}
+	}
+	public static void main(String args[]) {
+		char arr[][]={
+	               {'1','1','0','0','0'},
+	               {'0','1','1','0','0'},
+	               {'0','0','1','0','1'},
+	               {'1','0','0','0','1'},
+	               {'0','1','0','1','1'}
+	               };
+		System.out.println(numIslands(arr));
 	}
 }
